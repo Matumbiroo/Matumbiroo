36b578db6cae3ed13fe0a7c2b5961963e1afb9e4
hiimnate <nathanlwatts7@gmail.com>
2017-09-28 15:56:54 -0600

    set up index file with router, thunk, redux, store, and providers etc

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {connect} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import mainReducer from './redux/reducers/reducers';
+
 import App from './main/App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(mainReducer, composeWithDevTools(
+    applyMiddleware(thunk)
+));
+
+store.subscribe(()=> {
+    console.log(store.getState())
+});
+
+ReactDOM.render(<Provider store={store}><Router><App/></Router></Provider> ,document.getElementById('root'));
