6d5b5d667506c8449854a7c124036f5c22f2dfe8
hiimnate <nathanlwatts7@gmail.com>
2017-09-28 15:44:41 -0600

    basic file structure laid out

---
+++ client/src/main/routes/main-login/LoginContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginComponent from './LoginComponent';
+
+class LoginContainer extends React.Component {
+    render() {
+    return (
+        <LoginComponent/>
+        )
+    }
+}
+
+export default LoginContainer;
---
+++ client/src/main/routes/stat-display/StatDisplayContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StatDisplayComponent from "./StatDisplayComponent";
+
+class StatDisplayContainer extends React.Component {
+    render() {
+    return (
+        <StatDisplayComponent/>
+        )
+    }
+}
+
+export default StatDisplayContainer;
