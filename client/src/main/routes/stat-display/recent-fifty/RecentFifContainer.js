bee927d082dbd8fc79ab7995e53af4f7b5bfe1f7
hiimnate <nathanlwatts7@gmail.com>
2017-09-29 16:30:09 -0600

    created recent fifty page with basic layout and began working on action creator functions for getting recent fifty from spotify

--- client/src/main/routes/stat-display/recent-fifty/RecentFifContainer.js
+++ client/src/main/routes/stat-display/recent-fifty/RecentFifContainer.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import RecentFifComponent from "./RecentFifComponent";
+import {connect} from 'react-redux';
+import * as actionCreators from '../../../../redux/actions/actions';
 
 class RecentFifContainer extends React.Component {
     render() {
+        console.log(this.props);
     return (
-        <RecentFifComponent/>
+        <RecentFifComponent
+
+        />
         )
     }
 }
 
-export default RecentFifContainer;
+const mapStateToProps = (state) => {
+    return state;
+};
+
+export default connect(mapStateToProps, actionCreators)(RecentFifContainer);
