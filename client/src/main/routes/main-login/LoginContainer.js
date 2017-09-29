b465208e4b221581245aecaf381c74eab890be37
gavin-kemp <13gkemp@gmail.com>
2017-09-29 14:14:30 -0600

    styled login page

--- client/src/main/routes/main-login/LoginContainer.js
+++ client/src/main/routes/main-login/LoginContainer.js
@@ -2,9 +2,33 @@ import React from 'react';
 import LoginComponent from './LoginComponent';
 
 class LoginContainer extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            className: "signup-form hide"
+        }
+    }
+    showForm = () => {
+        this.setState(() => {
+            return {
+                className: "signup-form show-form"
+            }
+        })
+    }
+    hideForm = () => {
+        this.setState(() => {
+            return {
+                className: "signup-form hide"
+            }
+        })
+    }
     render() {
     return (
-        <LoginComponent/>
+        <LoginComponent
+        className = {this.state.className}
+        showForm = {this.showForm}
+        hideForm = {this.hideForm}
+        />
         )
     }
 }
