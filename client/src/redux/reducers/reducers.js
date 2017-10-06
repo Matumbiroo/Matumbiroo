762eb7f06867a1c034e6868fc90a3559e422670e
gavin-kemp <13gkemp@gmail.com>
2017-10-06 14:28:07 -0600

    started user authentication

--- client/src/redux/reducers/reducers.js
+++ client/src/redux/reducers/reducers.js
@@ -7,6 +7,18 @@ import {
 const initialState = {
     accessToken: null,
     refreshToken: null,
+    matumUser: {
+        isAuthenticated: false,
+        user: {
+            email:"",
+            username: ""
+        },
+        authError: {
+            signup: "",
+            login: "",
+            verify: ""
+        }
+    },
     user: {
         loading: false,
         country: null,
@@ -92,6 +104,27 @@ const mainReducer = function(state = initialState, action) {
                     }}]
                 }
             };
+        case "AUTHENTICATE":
+            return {
+                ...state.matumUser,
+                isAuthenticated: action.isValid,
+                user: action.user,
+                authError: {
+                    ...initialState.matumUser.authError
+                }
+            }
+        case "LOGOUT":
+            return {
+                ...initialState.matumUser
+            }
+        case "AUTH_ERROR":
+            return {
+                ...state.matumUser,
+                authError: {
+                    ...state.matumUser.authError,
+                    ...action.err
+                }
+            }
         default:
             return state;
     }
